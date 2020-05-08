bcf16492ebc4542d687e56353910aae7db023e1e
Ivan <ivanbeljan@net.hr>
2020-05-08 14:07:43 +0200

    destructured large function in several smaller ones, now each result is displayed as it's fetched instead of waiting for everything to be fetched

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var selectedFile;
 var outputHtml = [];
 var shipments = [];
+var result = [];
 let columnNumber;
 document
   .getElementById("fileUpload")
@@ -23,85 +24,89 @@ document.getElementById("uploadExcel").addEventListener("click", function () {
         );
         let jsonObject = JSON.stringify(rowObject);
         let parsedData = JSON.parse(jsonObject);
-        let result = parsedData.map((opa) => Object.values(opa)[columnNumber]);
-        (async () => {
-          spinner.removeAttribute("hidden");
-          for (let i = 0; i < result.length; i++) {
-            console.log(result[i]);
-            let index = result[i];
-            let res = await fetch(
-              "https://my.overseas.hr/system/api/track-and-trace/get-shipment-data/" +
-                index
-            );
-            let shipmentData = await res.json();
-            shipments.push(shipmentData);
-          }
-          spinner.setAttribute("hidden", "");
-          outputHtml = shipments
-            .map(
-              (shipment, index) => `
-        <div class="carc card-body mb-1">
-          <a href="#" class="js-modal-show" id="${index}" onClick="triggerPopup(this.id)"> <h4> ${
-                shipment.Ref2
-              } </h4> </a> <h4> <span class="text-warning" id="Color${
-                shipment.LastShipmentTrace.StatusNumber
-              }">${shipment.StatusDescription}</span></h4>
-        </div>
-        <div class="l-modal is-hidden--off-flow js-modal-shopify" id="test${index}">
-      <div class="l-modal__shadow js-modal-hide" id="hide${index}"></div>
-      <div class="c-popup l-modal__body" id="classFinder${index}">
-        <div class="align-left"> Broj narudžbe: ${
-          shipment.ShipmentNumber
-        } </div>
-        <div class="align-right"> Datum slanja: ${
-          shipment.SentOnDateString
-        } </div>
-        <h5 class="c-popup__title">${shipment.Ref2}</h5>
-        <p class="c-popup__description"> Trenutno stanje: </p>
-        <h5 class="c-popup__title1" id="align-left${index}"> ${
-                shipment.StatusDescription
-              } </h5>
-        <p class="c-popup__description"> ${
-          shipment.LastShipmentTrace.ScanDateString
-        }   ${shipment.LastShipmentTrace.ScanTimeString}</p>
-        <h6 class="c-popup__title">Primatelj: ${
-          shipment.Consignee.PostalCode
-        } ${shipment.Consignee.City}, ${shipment.Consignee.Country} </h6>
-          <table class="table">
-              <thead class="c-popup__description_table">
-                  <tr>
-                      <th scope="col"> Datum </th>
-                      <th scope="col"> Vrijeme</th>
-                      <th scope="col"> Status </th>
-                      <th scope="col"> Napomena </th>
-                  </tr>
-        <tbody class="c-popup__description_table"> ${shipment.Collies[0].Traces.map(
-          (track) =>
-            "<tr><td>" +
-            track.ScanDateString +
-            "</td><td>" +
-            track.ScanTimeString +
-            "</td><td>" +
-            track.StatusDescription +
-            "</td><td>" +
-            track.Remark +
-            "</td></tr>"
-        ).join("")}</tbody>
-
-        </table>
-        <input type="button" value="Zatvori prozor" id="${index}" onclick="closePopup(this.id)">
-      </div>
-        </div>
-        `
-            )
-            .join("");
-          document.getElementById("shipments-list").innerHTML = outputHtml;
-        })();
+        result = parsedData.map(
+          (result) => Object.values(result)[columnNumber]
+        );
+        fetchResult();
       });
     };
     fileReader.readAsBinaryString(selectedFile);
   }
 });
+
+const fetchResult = async () => {
+  spinner.removeAttribute("hidden");
+  for (let i = 0; i < result.length; i++) {
+    console.log(result[i]);
+    let index = result[i];
+    let res = await fetch(
+      "https://my.overseas.hr/system/api/track-and-trace/get-shipment-data/" +
+        index
+    );
+    let shipmentData = await res.json();
+    shipments.push(shipmentData);
+    DisplayData();
+  }
+};
+
+const DisplayData = () => {
+  spinner.setAttribute("hidden", "");
+  outputHtml = shipments
+    .map(
+      (shipment, index) => `
+  <div class="carc card-body mb-1">
+    <a href="#" class="js-modal-show" id="${index}" onClick="triggerPopup(this.id)"> <h4> ${
+        shipment.Ref2
+      } </h4> </a> <h4> <span class="text-warning" id="Color${
+        shipment.LastShipmentTrace.StatusNumber
+      }">${shipment.StatusDescription}</span></h4>
+  </div>
+  <div class="l-modal is-hidden--off-flow js-modal-shopify" id="test${index}">
+<div class="l-modal__shadow js-modal-hide" id="hide${index}"></div>
+<div class="c-popup l-modal__body" id="classFinder${index}">
+  <div class="align-left"> Broj narudžbe: ${shipment.ShipmentNumber} </div>
+  <div class="align-right"> Datum slanja: ${shipment.SentOnDateString} </div>
+  <h5 class="c-popup__title">${shipment.Ref2}</h5>
+  <p class="c-popup__description"> Trenutno stanje: </p>
+  <h5 class="c-popup__title1" id="align-left${index}"> ${
+        shipment.StatusDescription
+      } </h5>
+  <p class="c-popup__description"> ${
+    shipment.LastShipmentTrace.ScanDateString
+  }   ${shipment.LastShipmentTrace.ScanTimeString}</p>
+  <h6 class="c-popup__title">Primatelj: ${shipment.Consignee.PostalCode} ${
+        shipment.Consignee.City
+      }, ${shipment.Consignee.Country} </h6>
+    <table class="table">
+        <thead class="c-popup__description_table">
+            <tr>
+                <th scope="col"> Datum </th>
+                <th scope="col"> Vrijeme</th>
+                <th scope="col"> Status </th>
+                <th scope="col"> Napomena </th>
+            </tr>
+  <tbody class="c-popup__description_table"> ${shipment.Collies[0].Traces.map(
+    (track) =>
+      "<tr><td>" +
+      track.ScanDateString +
+      "</td><td>" +
+      track.ScanTimeString +
+      "</td><td>" +
+      track.StatusDescription +
+      "</td><td>" +
+      track.Remark +
+      "</td></tr>"
+  ).join("")}</tbody>
+
+  </table>
+  <input type="button" value="Zatvori prozor" id="${index}" onclick="closePopup(this.id)">
+</div>
+  </div>
+  `
+    )
+    .join("");
+  document.getElementById("shipments-list").innerHTML = outputHtml;
+};
 
 function triggerPopup(id) {
   let classElement = "#test" + id;
